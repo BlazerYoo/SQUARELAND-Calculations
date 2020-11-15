43cb77350db0e381012b1852ea4478f03c58e0a7
BlazerYoo <69565038+BlazerYoo@users.noreply.github.com>
2020-11-15 06:09:44 -0500

    Update convex_hull_calc.py

--- convex_hull_calc.py
+++ convex_hull_calc.py
@@ -1,15 +1,10 @@
-#https://stackoverflow.com/questions/19873596/convex-hull-area-in-python + my stuff
-import numpy as np
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.ConvexHull.html + my stuff
+from scipy.spatial import ConvexHull
 
-def conv_hull_area(point_arr):
-    lines = np.hstack([point_arr,np.roll(point_arr,-1,axis=0)])
-    area = 0.5*abs(sum(x1*y2-x2*y1 for x1,y1,x2,y2 in lines))
-    return area
-
-def conv_hull(area):
+def conv_hull(conv_hull_area):
   dis_area = float(input("District area: "))
-  conv_hull = dis_area/area
-  return conv_hull
+  conv_hull_score = dis_area/conv_hull_area
+  return conv_hull_score
 
 num_point = int(input("Num points: "))
 point_arr = []
@@ -18,8 +13,8 @@ for point in range(num_point):
   x = float(input("X: "))
   y = float(input("Y: "))
   point_arr.append([x, y])
-area = conv_hull_area(point_arr)
-conv_hull = conv_hull(area)
+hull = ConvexHull(point_arr)
+conv_hull_score = conv_hull(hull.area)
 print(point_arr)
-print("Area of convex hull:",round(area,7))
-print("Convex hull:",round(conv_hull,7))
+print("Area of convex hull: ",hull.area)
+print("Convex hull scores:",conv_hull_score)
