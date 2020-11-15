5cdda1a12ce6ab989b281a876ee4c162f8d292a4
BlazerYoo <69565038+BlazerYoo@users.noreply.github.com>
2020-11-15 06:09:27 -0500

    Update reock_calc.py

--- reock_calc.py
+++ reock_calc.py
@@ -1,60 +1,87 @@
-#https://www.geeksforgeeks.org/minimum-enclosing-circle-set-1/ + my stuff
-from math import sqrt, pi
+# https://www.nayuki.io/res/smallest-enclosing-circle/smallestenclosingcircle.py + mystuff
 
-INF = 10**18
+from math import pi
+import math, random
 
-def dist(a, b): 
-	return sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2)) 
+def make_circle(points):
+	shuffled = [(float(x), float(y)) for (x, y) in points]
+	random.shuffle(shuffled)
+	c = None
+	for (i, p) in enumerate(shuffled):
+		if c is None or not is_in_circle(c, p):
+			c = _make_circle_one_point(shuffled[ : i + 1], p)
+	return c
 
-def is_inside(c, p): 
-	return dist(c[0], p) <= c[1] 
+def _make_circle_one_point(points, p):
+	c = (p[0], p[1], 0.0)
+	for (i, q) in enumerate(points):
+		if not is_in_circle(c, q):
+			if c[2] == 0.0:
+				c = make_diameter(p, q)
+			else:
+				c = _make_circle_two_points(points[ : i + 1], p, q)
+	return c
 
-def get_circle_center(bx, by, cx, cy): 
-	B = bx * bx + by * by 
-	C = cx * cx + cy * cy 
-	D = bx * cy - by * cx 
-	return [(cy * B - by * C) // (2 * D), 
-			(bx * C - cx * B) // (2 * D) ] 
+def _make_circle_two_points(points, p, q):
+	circ = make_diameter(p, q)
+	left  = None
+	right = None
+	px, py = p
+	qx, qy = q
+	for r in points:
+		if is_in_circle(circ, r):
+			continue
+		cross = _cross_product(px, py, qx, qy, r[0], r[1])
+		c = make_circumcircle(p, q, r)
+		if c is None:
+			continue
+		elif cross > 0.0 and (left is None or _cross_product(px, py, qx, qy, c[0], c[1]) > _cross_product(px, py, qx, qy, left[0], left[1])):
+			left = c
+		elif cross < 0.0 and (right is None or _cross_product(px, py, qx, qy, c[0], c[1]) < _cross_product(px, py, qx, qy, right[0], right[1])):
+			right = c
+	if left is None and right is None:
+		return circ
+	elif left is None:
+		return right
+	elif right is None:
+		return left
+	else:
+		return left if (left[2] <= right[2]) else right
 
-def circle_frOm(A, B,C): 
-	I = get_circle_center(B[0] - A[0], B[1] - A[1], 
-								C[0] - A[0], C[1] - A[1]) 
-	I[0] += A[0] 
-	I[1] += A[1] 
-	return [I, dist(I, A)] 
+def make_diameter(a, b):
+	cx = (a[0] + b[0]) / 2
+	cy = (a[1] + b[1]) / 2
+	r0 = math.hypot(cx - a[0], cy - a[1])
+	r1 = math.hypot(cx - b[0], cy - b[1])
+	return (cx, cy, max(r0, r1))
 
-def circle_from(A, B): 
-	C = [ (A[0] + B[0]) / 2.0, (A[1] + B[1]) / 2.0] 
-	return [C, dist(A, B) / 2.0] 
 
-def is_valid_circle(c, P): 
-	for p in P: 
-		if (is_inside(c, p) == False): 
-			return False
-	return True
+def make_circumcircle(a, b, c):
+	ox = (min(a[0], b[0], c[0]) + max(a[0], b[0], c[0])) / 2
+	oy = (min(a[1], b[1], c[1]) + max(a[1], b[1], c[1])) / 2
+	ax = a[0] - ox;  ay = a[1] - oy
+	bx = b[0] - ox;  by = b[1] - oy
+	cx = c[0] - ox;  cy = c[1] - oy
+	d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2.0
+	if d == 0.0:
+		return None
+	x = ox + ((ax*ax + ay*ay) * (by - cy) + (bx*bx + by*by) * (cy - ay) + (cx*cx + cy*cy) * (ay - by)) / d
+	y = oy + ((ax*ax + ay*ay) * (cx - bx) + (bx*bx + by*by) * (ax - cx) + (cx*cx + cy*cy) * (bx - ax)) / d
+	ra = math.hypot(x - a[0], y - a[1])
+	rb = math.hypot(x - b[0], y - b[1])
+	rc = math.hypot(x - c[0], y - c[1])
+	return (x, y, max(ra, rb, rc))
 
-def minimum_enclosing_circle(P): 
-	n = len(P) 
-	if (n == 0): 
-		return [[0, 0], 0] 
-	if (n == 1): 
-		return [P[0], 0] 
-	mec = [[0, 0], INF] 
-	for i in range(n): 
-		for j in range(i + 1, n): 
-			tmp = circle_from(P[i], P[j]) 
-			if (tmp[1] < mec[1] and is_valid_circle(tmp, P)): 
-				mec = tmp 
-	for i in range(n): 
-		for j in range(i + 1, n): 
-			for k in range(j + 1, n): 
-				tmp = circle_frOm(P[i], P[j], P[k]) 
-				if (tmp[1] < mec[1] and is_valid_circle(tmp, P)): 
-					mec = tmp 
-	return mec 
+_MULTIPLICATIVE_EPSILON = 1 + 1e-14
 
-def area(mec):
-  circle_area = pi*mec[1]**2
+def is_in_circle(c, p):
+	return c is not None and math.hypot(p[0] - c[0], p[1] - c[1]) <= c[2] * _MULTIPLICATIVE_EPSILON
+
+def _cross_product(x0, y0, x1, y1, x2, y2):
+	return (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0)
+
+def area(radius):
+  circle_area = pi*radius**2
   return circle_area
 
 def reock(circle_area):
@@ -68,11 +95,12 @@ for point in range(num_point):
   print("Point " + str(point + 1))
   x = float(input("X: "))
   y = float(input("Y: "))
-  point_arr.append([x, y])
-mec = minimum_enclosing_circle(point_arr)
-circle_area = area(mec)
+  point_tuple = (x,y)
+  point_arr.append(point_tuple)
+circle_info = make_circle(point_arr)
+circle_area = area(circle_info[-1])
 reock_score = reock(circle_area)
 print(point_arr)
-print("Center = (",mec[0][0],",",mec[0][1],") Radius = ",round(mec[1],7)) 
+print(circle_info)
 print("Area of minimum enclosing circle:",round(circle_area,7))
-print("Reok score:", round(reock_score,7))
+print("Reock score:", round(reock_score,7))
